f7bcdd2b2151a694c70903b08c29b5eed60b227a
suraj-mulchandani-itp <suraj.mulchandani@intuitive.cloud>
2023-04-10 15:35:43 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,46 @@
+resource "azurerm_resource_group" "fd_resource_group" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_frontdoor" "fdwert" {
+  name                = "fd-wert"
+  resource_group_name = var.resource_group_name
+
+  routing_rule {
+    name               = "exampleRoutingRule1"
+    accepted_protocols = ["Http", "Https"]
+    patterns_to_match  = ["/*"]
+    frontend_endpoints = ["fd-wert"]
+    forwarding_configuration {
+      forwarding_protocol = "MatchRequest"
+      backend_pool_name   = "exampleBackendBing"
+    }
+  }
+
+  backend_pool_load_balancing {
+    name = "exampleLoadBalancingSettings1"
+  }
+
+  backend_pool_health_probe {
+    name = "exampleHealthProbeSetting1"
+  }
+
+  backend_pool {
+    name = "exampleBackendBing"
+    backend {
+      host_header = "www.bing.com"
+      address     = "www.bing.com"
+      http_port   = 80
+      https_port  = 443
+    }
+
+    load_balancing_name = "exampleLoadBalancingSettings1"
+    health_probe_name   = "exampleHealthProbeSetting1"
+  }
+
+  frontend_endpoint {
+    name      = "fd-wert"
+    host_name = "fd-wert.azurefd.net"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  type    = string
+  default = "az_fd_rg_01"
+}
+
+variable "location" {
+  type    = string
+  default = "Central India"
+}
